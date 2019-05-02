5316cca1adf03f97addeddfdc265c8481a66d347
walkiriapm@gmail.com <bayer868>
2019-05-02 21:26:18 +0200

    carousel

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 function hideTopPage () {
-    document.getElementById('').classList.add('hidden')
+    var button1 = document.getElementById('button-left')
+    var button2 = document.getElementById('button-right')
+    var image1 = document.getElementById('user-journey')
+    var image2 = document.getElementById('user-journey2')
+
+    if(image1.classList.contains('hidden')) {
+        button1.classList.remove('hidden')
+        button2.classList.add('hidden')
+        image1.classList.remove('hidden')
+        image2.classList.add('hidden')
+    } else {
+        button1.classList.add('hidden')
+        button2.classList.remove('hidden')
+        image1.classList.add('hidden')
+        image2.classList.remove('hidden')
+    }
 }
+
