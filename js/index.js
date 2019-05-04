721fd58d51f123631c9e4487a874d76c011ecd57
walkiriapm@gmail.com <bayer868>
2019-05-04 11:39:44 +0200

    cambios-listo-presentacion

--- js/index.js
+++ js/index.js
@@ -17,3 +17,25 @@ function hideTopPage () {
     }
 }
 
+
+function hideTopPage2 () {
+    var button1 = document.getElementById('button-left2')
+    var button2 = document.getElementById('button-right2')
+    var image1 = document.getElementById('mind-map-img')
+    var image2 = document.getElementById('empathy-map')
+
+    if(image1.classList.contains('hidden')) {
+        button1.classList.remove('hidden')
+        button2.classList.add('hidden')
+        image1.classList.remove('hidden')
+        image2.classList.add('hidden')
+    } else {
+        button1.classList.add('hidden')
+        button2.classList.remove('hidden')
+        image1.classList.add('hidden')
+        image2.classList.remove('hidden')
+    }
+}
+
+
+
